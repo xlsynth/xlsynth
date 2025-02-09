425ddc65a463d072d632e2a728e780cd2c1db280
Chris Leary <cdleary@openai.com>
2025-02-09 08:13:59 +0000

    [DSLX:TS] Fix bug with exhaustiveness sampling enum from another module.

--- xls/dslx/exhaustiveness/exhaustiveness_match_test.cc
+++ xls/dslx/exhaustiveness/exhaustiveness_match_test.cc
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "gtest/gtest.h"
 #include "absl/log/log.h"
 #include "absl/status/status_matchers.h"
+#include "gtest/gtest.h"
 #include "xls/common/status/matchers.h"
 #include "xls/common/visitor.h"
 #include "xls/dslx/create_import_data.h"
