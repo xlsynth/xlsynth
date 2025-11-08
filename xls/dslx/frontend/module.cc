google/xls

Add helper for specializing parametric functions

xls/dslx/frontend/module.cc
@@ -15,6 +15,7 @@
 #include "xls/dslx/frontend/module.h"
 
 #include <filesystem>
+#include <iterator>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -215,6 +216,47 @@ std::vector<std::string> Module::GetFunctionNames() const {
   return result;
 }
 
+std::vector<std::string> Module::GetMemberNames(
+    const ModuleMember& member) const {
+  return absl::visit(
+      Visitor{
+          [](Function* f) { return std::vector<std::string>{f->identifier()}; },
+          [](Proc* p) { return std::vector<std::string>{p->identifier()}; },
+          [](TestFunction* tf) {
+            return std::vector<std::string>{tf->identifier()};
+          },
+          [](TestProc* tp) {
+            return std::vector<std::string>{tp->proc()->identifier()};
+          },
+          [](QuickCheck* qc) {
+            return std::vector<std::string>{qc->identifier()};
+          },
+          [](TypeAlias* td) {
+            return std::vector<std::string>{td->identifier()};
+          },
+          [](StructDef* sd) {
+            return std::vector<std::string>{sd->identifier()};
+          },
+          [](ProcDef* pd) {
+            return std::vector<std::string>{pd->identifier()};
+          },
+          [](Impl* /*id*/) { return std::vector<std::string>{}; },
+          [](ConstantDef* cd) {
+            return std::vector<std::string>{cd->identifier()};
+          },
+          [](EnumDef* ed) {
+            return std::vector<std::string>{ed->identifier()};
+          },
+          [](Import* i) { return std::vector<std::string>{i->identifier()}; },
+          [](Use* u) {
+            return std::vector<std::string>{u->GetLeafIdentifiers()};
+          },
+          [](VerbatimNode*) { return std::vector<std::string>{}; },
+          [](ConstAssert* /*n*/) { return std::vector<std::string>{}; },
+      },
+      member);
+}
+
 const StructDef* Module::FindStructDef(const Span& span) const {
   return down_cast<const StructDef*>(FindNode(AstNodeKind::kStructDef, span));
 }
@@ -351,43 +393,7 @@ absl::StatusOr<TypeDefinition> Module::GetTypeDefinition(
 absl::Status Module::AddTop(ModuleMember member,
                             const MakeCollisionError& make_collision_error) {
   // Get name
-  std::vector<std::string> member_names = absl::visit(
-      Visitor{
-          [](Function* f) { return std::vector<std::string>{f->identifier()}; },
-          [](Proc* p) { return std::vector<std::string>{p->identifier()}; },
-          [](TestFunction* tf) {
-            return std::vector<std::string>{tf->identifier()};
-          },
-          [](TestProc* tp) {
-            return std::vector<std::string>{tp->proc()->identifier()};
-          },
-          [](QuickCheck* qc) {
-            return std::vector<std::string>{qc->identifier()};
-          },
-          [](TypeAlias* td) {
-            return std::vector<std::string>{td->identifier()};
-          },
-          [](StructDef* sd) {
-            return std::vector<std::string>{sd->identifier()};
-          },
-          [](ProcDef* pd) {
-            return std::vector<std::string>{pd->identifier()};
-          },
-          [](Impl* id) { return std::vector<std::string>{}; },
-          [](ConstantDef* cd) {
-            return std::vector<std::string>{cd->identifier()};
-          },
-          [](EnumDef* ed) {
-            return std::vector<std::string>{ed->identifier()};
-          },
-          [](Import* i) { return std::vector<std::string>{i->identifier()}; },
-          [](Use* u) {
-            return std::vector<std::string>{u->GetLeafIdentifiers()};
-          },
-          [](VerbatimNode*) { return std::vector<std::string>{}; },
-          [](ConstAssert* n) { return std::vector<std::string>{}; },
-      },
-      member);
+  std::vector<std::string> member_names = GetMemberNames(member);
 
   for (const std::string& member_name : member_names) {
     if (top_by_name_.contains(member_name)) {
@@ -406,6 +412,54 @@ absl::Status Module::AddTop(ModuleMember member,
   }
 
   top_.push_back(member);
+  top_set_.insert(ToAstNode(member));
+  for (const std::string& member_name : member_names) {
+    top_by_name_.insert({member_name, member});
+  }
+  return absl::OkStatus();
+}
+
+absl::Status Module::InsertTopAfter(
+    const AstNode* target_member, ModuleMember member,
+    const MakeCollisionError& make_collision_error) {
+  CHECK_NE(target_member, nullptr);
+  if (!top_set_.contains(target_member)) {
+    return absl::NotFoundError(
+        absl::StrFormat("Target member is not part of module %s", name_));
+  }
+
+  std::vector<std::string> member_names = GetMemberNames(member);
+
+  for (const std::string& member_name : member_names) {
+    if (top_by_name_.contains(member_name)) {
+      const AstNode* node = ToAstNode(top_by_name_.at(member_name));
+      const Span existing_span = node->GetSpan().value();
+      const AstNode* new_node = ToAstNode(member);
+      const Span new_span = new_node->GetSpan().value();
+      if (make_collision_error != nullptr) {
+        return make_collision_error(name_, member_name, existing_span, node,
+                                    new_span, new_node);
+      }
+      return absl::InvalidArgumentError(absl::StrFormat(
+          "Module %s already contains a member named %s @ %s: %s", name_,
+          member_name, existing_span.ToString(*file_table_), node->ToString()));
+    }
+  }
+
+  auto insert_it = top_.end();
+  for (auto it = top_.begin(); it != top_.end(); ++it) {
+    if (ToAstNode(*it) == target_member) {
+      insert_it = std::next(it);
+      break;
+    }
+  }
+
+  if (insert_it == top_.end()) {
+    top_.push_back(member);
+  } else {
+    insert_it = top_.insert(insert_it, member);
+  }
+
   top_set_.insert(ToAstNode(member));
   for (const std::string& member_name : member_names) {
     top_by_name_.insert({member_name, member});

xlsynth/xlsynth

xls/dslx/frontend/module.cc
@@ -15,6 +15,7 @@
 #include "xls/dslx/frontend/module.h"
 
 #include <filesystem>
+#include <iterator>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -215,6 +216,21 @@ std::vector<std::string> Module::GetFunctionNames() const {
   return result;
 }
 
+std::vector<std::string> Module::GetMemberNames(
+    const ModuleMember& member) const {
+  return absl::visit(
+      Visitor{
+        [](auto* m) { return std::vector<std::string>{m->identifier()}; },
+        [](Impl* id) { return std::vector<std::string>{}; },
+        [](Use* u) {
+          return std::vector<std::string>{u->GetLeafIdentifiers()};
+        },
+        [](VerbatimNode*) { return std::vector<std::string>{}; },
+        [](ConstAssert* n) { return std::vector<std::string>{}; },
+      },
+      member);
+}
+
 const StructDef* Module::FindStructDef(const Span& span) const {
   return down_cast<const StructDef*>(FindNode(AstNodeKind::kStructDef, span));
 }
@@ -351,17 +367,7 @@ absl::StatusOr<TypeDefinition> Module::GetTypeDefinition(
 absl::Status Module::AddTop(ModuleMember member,
                             const MakeCollisionError& make_collision_error) {
   // Get name
-  std::vector<std::string> member_names = absl::visit(
-      Visitor{
-          [](auto* m) { return std::vector<std::string>{m->identifier()}; },
-          [](Impl* id) { return std::vector<std::string>{}; },
-          [](Use* u) {
-            return std::vector<std::string>{u->GetLeafIdentifiers()};
-          },
-          [](VerbatimNode*) { return std::vector<std::string>{}; },
-          [](ConstAssert* n) { return std::vector<std::string>{}; },
-      },
-      member);
+  std::vector<std::string> member_names = GetMemberNames(member);
 
   for (const std::string& member_name : member_names) {
     if (top_by_name_.contains(member_name)) {
@@ -380,6 +386,54 @@ absl::Status Module::AddTop(ModuleMember member,
   }
 
   top_.push_back(member);
+  top_set_.insert(ToAstNode(member));
+  for (const std::string& member_name : member_names) {
+    top_by_name_.insert({member_name, member});
+  }
+  return absl::OkStatus();
+}
+
+absl::Status Module::InsertTopAfter(
+    const AstNode* target_member, ModuleMember member,
+    const MakeCollisionError& make_collision_error) {
+  CHECK_NE(target_member, nullptr);
+  if (!top_set_.contains(target_member)) {
+    return absl::NotFoundError(
+        absl::StrFormat("Target member is not part of module %s", name_));
+  }
+
+  std::vector<std::string> member_names = GetMemberNames(member);
+
+  for (const std::string& member_name : member_names) {
+    if (top_by_name_.contains(member_name)) {
+      const AstNode* node = ToAstNode(top_by_name_.at(member_name));
+      const Span existing_span = node->GetSpan().value();
+      const AstNode* new_node = ToAstNode(member);
+      const Span new_span = new_node->GetSpan().value();
+      if (make_collision_error != nullptr) {
+        return make_collision_error(name_, member_name, existing_span, node,
+                                    new_span, new_node);
+      }
+      return absl::InvalidArgumentError(absl::StrFormat(
+          "Module %s already contains a member named %s @ %s: %s", name_,
+          member_name, existing_span.ToString(*file_table_), node->ToString()));
+    }
+  }
+
+  auto insert_it = top_.end();
+  for (auto it = top_.begin(); it != top_.end(); ++it) {
+    if (ToAstNode(*it) == target_member) {
+      insert_it = std::next(it);
+      break;
+    }
+  }
+
+  if (insert_it == top_.end()) {
+    top_.push_back(member);
+  } else {
+    insert_it = top_.insert(insert_it, member);
+  }
+
   top_set_.insert(ToAstNode(member));
   for (const std::string& member_name : member_names) {
     top_by_name_.insert({member_name, member});
212f1543bffcc6a79953e8ed0c6d49300bd42445
Sirui Lu <siruilu@openai.com>
2025-09-30 13:10:25 -0700

    Add helper for specializing parametric functions

--- xls/dslx/frontend/ast.h
+++ xls/dslx/frontend/ast.h
@@ -2952,6 +2952,7 @@ class Slice : public AstNode {
 
   std::string ToString() const override;
   std::optional<Span> GetSpan() const override { return span_; }
+  const Span& span() const { return span_; }
 
   Expr* start() const { return start_; }
   Expr* limit() const { return limit_; }
@@ -3545,6 +3546,7 @@ class WidthSlice : public AstNode {
   Expr* start() const { return start_; }
   TypeAnnotation* width() const { return width_; }
   std::optional<Span> GetSpan() const override { return span_; }
+  const Span& span() const { return span_; }
 
  private:
   Span span_;
